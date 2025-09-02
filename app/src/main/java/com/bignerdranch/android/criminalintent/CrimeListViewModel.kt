0794597b019bfbe56df03bc9e83cc75800bb9bf7
T-a-r-i-q <tna003@student.usc.edu.au>
2025-09-02 21:49:30 +1000

    currently functioning with navigation

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,45 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.Date
+import java.util.UUID
+
+class CrimeListViewModel: ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
+
+    init{
+        viewModelScope.launch {
+            crimeRepository.getCrimes().collect {
+                _crimes.value = it
+            }
+
+        }
+    }
+
+
+
+
+
+
+    /*init {
+        for (i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title ="Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+            crimes += crime
+        }
+    }*/
+
+}
