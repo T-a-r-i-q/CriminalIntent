bdf84a12f78c8c6a150471a2937f40e5924067a1
T-a-r-i-q <tna003@student.usc.edu.au>
2025-09-09 22:09:26 +1000

    Implemented date selection with database updating.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -7,16 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
-private const val TAG = "CrimeDetailFragment"
+
 
 class CrimeDetailFragment: Fragment() {
-    private lateinit var crime: Crime
+
     private val args: CrimeDetailFragmentArgs by navArgs()
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
+    }
+
 
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
@@ -24,16 +35,7 @@ class CrimeDetailFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        crime = Crime(
-            id = UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
-        Log.d(TAG, "The crime ID is: ${args.crimeId}")
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,22 +51,55 @@ class CrimeDetailFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(title = text.toString())
+                }
             }
-            crimeDate.apply {
-                text = crime.date.toString()
-                isEnabled = false
-            }
+
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(isSolved = isChecked)
+                }
+
             }
 
         }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                crimeDetailViewModel.crime.collect { crime ->
+                    crime?.let { updateUi(it) }
+                }
+            }
+        }
+        setFragmentResultListener(
+            DatePickerFragment.REQUEST_KEY_DATE
+        ) { _, bundle ->
+            val newDate =
+                bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
+        }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(
+                    CrimeDetailFragmentDirections.selectDate(crime.date)
+                )
+            }
+            crimeSolved.isChecked = crime.isSolved
+        }
     }
 
 
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -2,19 +2,29 @@ package com.bignerdranch.android.criminalintent
 
 import android.content.Context
 import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 
 private const val DATABASE_NAME = "crime-database"
 
-class CrimeRepository private constructor(context: Context) {
+class CrimeRepository private constructor(context: Context, private val coroutineScope: CoroutineScope = GlobalScope) {
     private val database: CrimeDatabase= Room.databaseBuilder(
         context.applicationContext, CrimeDatabase::class.java, DATABASE_NAME
         ).createFromAsset(DATABASE_NAME).build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDAO().getCrimes()
     suspend fun getCrime(id: UUID): Crime = database.crimeDAO().getCrime(id)
+    //crimeDAO was listed Dao in text, this created error hence DAO
+    fun updateCrime(crime: Crime) {
+        coroutineScope.launch {
+            database.crimeDAO().updateCrime(crime)
+        }
+    }
+
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
